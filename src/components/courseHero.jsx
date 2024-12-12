9efb6ad6f3e52592232b09261e472594c3f07ff7
AmullyaPatil <patilamullya@gmail.com>
2024-12-12 19:06:22 +0530

    course pg updated

--- src/components/courseHero.jsx
+++ src/components/courseHero.jsx
@@ -5,22 +5,27 @@ import image from "../Assets/courses-img/coursehero.png";
 const coursehero = () => {
   return (
     <div className="coursehero-container">
+      <div> 
+      <p className="coursehero-pagename">Home |<span> Courses</span> </p>
+      </div>
+      <div className="coursehero-sub-container">
       <div className="coursehero-content">
-        <p className="coursehero-pagename">Home |<span> Courses</span> </p>
+        
         <h1 className="coursehero-headline1">
           Empowering</h1>
           <h1 className="coursehero-headline2"> <br/><span> Tomorrow's <br/> Leaders,</span>Today!
         </h1>
         <p className="coursehero-subheadline">
-          Transformative courses that go beyond <br/> traditional learning.
+          Transformative courses that go beyond  traditional learning.
         </p>
         <p className="coursehero-description">
-          Our programs cater to young minds from <br/> all walks of life.
+          Our programs cater to young minds from all walks of life.
         </p>
       </div>
       <div className="coursehero-image">
         <img src={image} alt="About Us" />
       </div>
+    </div>
     </div>
   );
 };
