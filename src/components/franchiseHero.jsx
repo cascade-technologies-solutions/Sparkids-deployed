f12972bc4a15f3c766b5fa0e3f7ec8c0d33ba20a
AmullyaPatil <patilamullya@gmail.com>
2024-12-12 19:05:55 +0530

    franchise responsive

--- src/components/franchiseHero.jsx
+++ src/components/franchiseHero.jsx
@@ -5,14 +5,19 @@ import image from "../Assets/franchise-img/franchisehero.png";
 const franchisehero = () => {
   return (
     <div className="franchisehero-container">
-      <div className="franchisehero-content">
+      <div>
         <p className="franchisehero-pagename">Home |<span>Franchise</span> </p>
+        </div>
+      {/* <div className="franchisehero-content"> */}
+        
+        <div className="franchisehero-sub-container">
+           <div className="franchisehero-content"> 
         <h1 className="franchisehero-headline1">
           Build a Bright future</h1>
           <h1 className="franchisehero-headline2"> <br/><span> Partner with </span><br/>Brave Sparkids<br/>Academy <span>Today!</span>
         </h1>
         <p className="franchisehero-subheadline">
-          Be part of a proven, profitable, and impactful <br/> franchise model.
+          Be part of a proven, profitable, and impactful franchise model.
         </p>
         <br/>
         <a href="#know-more" className="franchisehero-button">
@@ -22,6 +27,7 @@ const franchisehero = () => {
       <div className="franchisehero-image">
         <img src={image} alt="franchiseimg" />
       </div>
+      </div> 
     </div>
   );
 };
