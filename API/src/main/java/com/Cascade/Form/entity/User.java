e03052f4a1f51dce4ac41f6eeac9115fa6bae2c2
ash180403 <ashishakteri@gmail.com>
2024-12-23 21:59:05 +0530

    API

---
+++ API/src/main/java/com/Cascade/Form/entity/User.java
@@ -0,0 +1,57 @@
+package com.Cascade.Form.entity;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "users")
+public class User {
+
+    @Id
+    private String id;
+
+    private String name;
+    private String contact;
+    private String address;
+    private String courses;  
+
+    // Getters and setters
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getContact() {
+        return contact;
+    }
+
+    public void setContact(String contact) {
+        this.contact = contact;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCourses() {
+        return courses;
+    }
+
+    public void setCourses(String courses) {
+        this.courses = courses;
+    }
+}
