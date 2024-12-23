ca75911898250e6bc15f94054585788a1826d8f5
ash180403 <ashishakteri@gmail.com>
2024-12-23 22:52:29 +0530

    API

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Header from './components/Header';
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Footer from './components/Footer';
 import HomePage from "./pages/HomePage";
 import AboutPage from "./pages/AboutPage";
