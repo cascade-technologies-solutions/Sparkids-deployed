61104099b34dd7d85865b4a178bdccd051866f21
ash180403 <ashishakteri@gmail.com>
2024-12-24 23:15:13 +0530

    API

--- API/src/main/java/com/Cascade/Form/Controller/UserController.java
+++ API/src/main/java/com/Cascade/Form/Controller/UserController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.ui.Model;
 
-@Controller
+@RestController
 @CrossOrigin(origins = "http://localhost:3000")
 public class UserController {
 
