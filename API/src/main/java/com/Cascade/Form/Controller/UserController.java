6de23783a45eb121e8533f397e265388d0b847d2
ash180403 <ashishakteri@gmail.com>
2024-12-25 23:56:07 +0530

    Email API

--- API/src/main/java/com/Cascade/Form/Controller/UserController.java
+++ API/src/main/java/com/Cascade/Form/Controller/UserController.java
@@ -8,7 +8,6 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
