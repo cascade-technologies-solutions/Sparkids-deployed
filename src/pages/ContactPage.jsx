9823958141a6832e9fe0b9a71a01d5ffac11a3a7
ash180403 <ashishakteri@gmail.com>
2024-12-26 22:59:38 +0530

    Contact

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+//import Header from '../components/Header';
 //import Footer from '../components/Footer';
 import ContactForm from '../components/ContactForm';
 export const Contact = () => {
