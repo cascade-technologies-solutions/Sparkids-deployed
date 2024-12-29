5020ec528b788cc29f4e03c860f5254acaabcfcb
ash180403 <ashishakteri@gmail.com>
2024-12-29 17:00:06 +0530

    Contact

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -108,33 +108,6 @@ const ContactForm = () => {
           </form>
           {responseMessage && <p className="response-message">{responseMessage}</p>}
         </div>
-        {/* Right side - Contact Info
-        <div className="contact-info-section">
-          <h2>Contact us directly</h2>
-          <p><span>Call:</span> <br /> +91 82170 77217</p>
-          <p><span>Email:</span> <br /> example@mail.com</p>
-          <p><span>Address:</span> <br />
-            Indi Regional Office <br />
-            Behind BSNL Tower, Near Mallikarjun Temple, <br />
-            Vidya Nagar, Sindagi Road, Indi - 586209
-          </p>
-          <p><span>Working hours:</span> <br />
-            Open Weekly Mon - Friday <br />
-            9:00 AM to 5:30 PM
-          </p>
-          <a href="#map" className="navigate-link">Navigate to head branch</a>
-          <div className="map">
-            <iframe
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3153.8354345093785!2d144.96305791531665!3d-37.81410797975186!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6ad642af0f11fd81%3A0xf577d9b7b78a9b0!2sFederation%20Square!5e0!3m2!1sen!2sau!4v1635828576584!5m2!1sen!2sau"
-              width="100%"
-              height="300"
-              style={{ border: 0, borderRadius: '5px' }}
-              allowFullScreen=""
-              loading="lazy"
-              title="Google Map"
-            ></iframe>
-          </div> */}
-        {/* </div> */}
 
         {/* Right side - Contact Info */}
         <div className="contact-info-section">
